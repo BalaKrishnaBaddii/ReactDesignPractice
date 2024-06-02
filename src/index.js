25a0a18760f7222d2bfaa2d2137ddab71d7b3545
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-02 19:19:06 +0530

    modified codes as per props model

--- src/index.js
+++ src/index.js
@@ -4,99 +4,49 @@ import "./index.css";
 
 function App() {
   return (
-    <div>
-      {/* <Box /> */}
-      <Emojis />
+    <div className="box-main">
+      <Avatar />
+      <div className="bottom">
+        <Intro />
+        <Skillset />
+      </div>
     </div>
   );
 }
-function Emojis() {
-  return <Box imageName="htmlcss" emoji="./images/html_css.svg" />;
+
+function Avatar() {
+  return <img className="profile" src="./images/image1.jpg" alt="profile" />;
 }
-function Box(props) {
+function Intro() {
   return (
-    <div className="box-main">
-      <div className="top">
-        <img className="profile" src="./images/image1.jpg" alt="profile" />
-      </div>
-      <div className="bottom">
-        <h1 className="profile-name">Bala Krishna Baddi </h1>
-        <p>
-          Hey i'm a full stack developer with 3 years of industrial experience.
-          i worked almost ever cornor of the web technology. I Worked at PWc,
-          Amazon, NetFlix etc technologies along React.js angular java script
-          node.js etc..
-        </p>
-        <div className="tags-block">
-          <div>
-            <div className="tag-name" id="one">
-              <div>HTML+CSS</div>
-              <img
-                className="tag-emoji"
-                id="emoji-1"
-                src={props.emoji}
-                alt={props.imageName}
-              />
-            </div>
-          </div>
-          <div>
-            <div className="tag-name" id="two">
-              <div>Java Script</div>
-              <img
-                className="tag-emoji"
-                id="emoji-1"
-                src={props.emoji}
-                alt={props.imageName}
-              />
-            </div>
-          </div>
-          <div>
-            <div className="tag-name" id="three">
-              <div>Web Design</div>
-              <img
-                className="tag-emoji"
-                id="emoji-1"
-                src={props.emoji}
-                alt={props.imageName}
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="tag-name" id="four">
-              <div>Git and GitHub</div>
-              <img
-                className="tag-emoji"
-                id="emoji-1"
-                src={props.emoji}
-                alt={props.imageName}
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="tag-name" id="five">
-              <div>React</div>
-              <img
-                className="tag-emoji"
-                id="emoji-1"
-                src={props.emoji}
-                alt={props.imageName}
-              />
-            </div>
-          </div>
-
-          <div className="tag-name" id="six">
-            <div>Swelte</div>
-            <img
-              className="tag-emoji"
-              id="emoji-1"
-              src={props.emoji}
-              alt={props.imageName}
-            />
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1 className="profile-name">Bala Krishna Baddi </h1>
+      <p>
+        Hey i'm a full stack developer with 3 years of industrial experience. i
+        worked almost ever cornor of the web technology. I Worked at PWc,
+        Amazon, NetFlix etc technologies along React.js angular java script
+        node.js etc..
+      </p>
+    </div>
+  );
+}
+function Skillset() {
+  return (
+    <div className="tags-block">
+      <Skill id="one" skill="HTML&CSS" emoji="💪" />
+      <Skill id="two" skill="Java Script" emoji="💪" />
+      <Skill id="three" skill="Web Design" emoji="💪" />
+      <Skill id="four" skill="Git and Github" emoji="💪" />
+      <Skill id="five" skill="React" emoji="💪" />
+      <Skill id="six" skill="Sweltie" emoji="💪" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="tag-name" id={props.id}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
