0c0edf182d9beae1f330af93e57975c92bdf490f
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-02 16:25:05 +0530

    My first Page Design

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      {/* <Box /> */}
+      <Emojis />
+    </div>
+  );
+}
+function Emojis() {
+  return <Box imageName="htmlcss" emoji="./images/html_css.svg" />;
+}
+function Box(props) {
+  return (
+    <div className="box-main">
+      <div className="top">
+        <img className="profile" src="./images/image1.jpg" alt="profile" />
+      </div>
+      <div className="bottom">
+        <h1 className="profile-name">Bala Krishna Baddi </h1>
+        <p>
+          Hey i'm a full stack developer with 3 years of industrial experience.
+          i worked almost ever cornor of the web technology. I Worked at PWc,
+          Amazon, NetFlix etc technologies along React.js angular java script
+          node.js etc..
+        </p>
+        <div className="tags-block">
+          <div>
+            <div className="tag-name" id="one">
+              <div>HTML+CSS</div>
+              <img
+                className="tag-emoji"
+                id="emoji-1"
+                src={props.emoji}
+                alt={props.imageName}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="tag-name" id="two">
+              <div>Java Script</div>
+              <img
+                className="tag-emoji"
+                id="emoji-1"
+                src={props.emoji}
+                alt={props.imageName}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="tag-name" id="three">
+              <div>Web Design</div>
+              <img
+                className="tag-emoji"
+                id="emoji-1"
+                src={props.emoji}
+                alt={props.imageName}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="tag-name" id="four">
+              <div>Git and GitHub</div>
+              <img
+                className="tag-emoji"
+                id="emoji-1"
+                src={props.emoji}
+                alt={props.imageName}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="tag-name" id="five">
+              <div>React</div>
+              <img
+                className="tag-emoji"
+                id="emoji-1"
+                src={props.emoji}
+                alt={props.imageName}
+              />
+            </div>
+          </div>
+
+          <div className="tag-name" id="six">
+            <div>Swelte</div>
+            <img
+              className="tag-emoji"
+              id="emoji-1"
+              src={props.emoji}
+              alt={props.imageName}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
